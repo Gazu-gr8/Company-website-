4cc623b630e12636aec0c34bc339e86693df93ca
Gazu-gr8 <53431662+Gazu-gr8@users.noreply.github.com>
2019-12-03 10:21:30 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+<script>
+$(document).ready(function(){
+  $(".col").hover(function(){
+    $(this).css("background-color", "cream");
+    }, function(){
+    $(this).css("background-color", "light-grey");
+  });
+});
+</script>
